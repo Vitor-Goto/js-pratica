adda769f69749db7207a804de5d15f7a5e716110
Vitor-Goto <vitoryukiobomfim@gmail.com>
2022-08-19 18:26:01 -0300

    ajax axios fetch

--- ajax/server.js
+++ ajax/server.js
@@ -29,5 +29,23 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) =>{
+    res.send({
+        ...req.body,
+        id: 7 
+    })
+})
+
 app.get('/teste', (req, res) => res.send(new Date))
+
+app.get('/parOuImpar', (req, res) => {
+    //req.body
+    //req.query
+    //req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log ('Executando...'))
