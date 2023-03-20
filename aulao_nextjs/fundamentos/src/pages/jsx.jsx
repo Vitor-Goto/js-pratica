abb8c65235346cee694edbc55a86a562c48fa855
Vitor-Goto <102625930+Vitor-Goto@users.noreply.github.com>
2023-03-20 16:00:51 -0300

    next

---
+++ aulao_nextjs/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,11 @@
+export default function Jsx() {
+    const a = 2 
+    const b = 2
+    console.log(a * b)
+    return (
+        <div>
+            <h1>JSX é um conceito central</h1>
+            {Math.random()}
+        </div>
+    )
+}
